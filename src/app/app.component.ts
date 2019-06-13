03b111c9195814449f9007289dd2bbc47c29e609
immsee098 <immsee098@gmail.com>
2019-06-13 17:19:58 +0900

    ..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { generate } from 'rxjs';
+
+interface Todo{
+  id: number;
+  content: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-angular';
+  todos: Todo[] = [
+    {id: 1, content: "HTML", completed: false}
+  ]
+
+  addTodo(inputElem: HTMLInputElement){
+    console.log('aaaaa');
+    this.todos = [{id: this.generateId(), content: inputElem.value, completed: false}, ...this.todos]
+  }
+
+  removeTodo(id){
+    console.log('bbbb');
+    this.todos = this.todos.filter(todo => todo.id !== id)
+  }
+
+  generateId(){
+    return this.todos.length ? Math.max(...this.todos.map(todo => todo.id))+1 : 1;
+  }
 }
